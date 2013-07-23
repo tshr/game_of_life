ad1158128e9992032aa5f722d2dcdd2cf73e2452
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-07-22 20:42:33 -0700

    turn world into a class with constructor, make updateCells a method on world, move event key press listener into own function

--- game_of_life.coffee
+++ game_of_life.coffee
@@ -14,60 +14,68 @@ $ ->
   tick_interval = 50
   paper = Raphael("canvas", canvas_width, canvas_height)
   alive_color = "#50c0a8"
-  world = []
+  world = null
 
-  ### Functions ###
+  ### Classes ###
 
-  generateWorld = ->
+  class World
 
-    world.getCellByCoord = (row, column) ->
-      cell_id_string = "cell_" + row + "_" + column
-      $("#" + cell_id_string)
-
-    world.drawCell = (row, column, alive) ->
-      cell = @getCellByCoord(row, column)
-      if alive then cell.attr fill: alive_color else cell.attr fill: "#ffffff"
-
-    world.draw = ->
-      @drawCell(j, k, world[j][k].alive) for k in [0...num_columns] for j in [0...num_rows]
-
-    createRaphaelCell = (row, column, height, width) ->
-      cell = paper.rect(column * width, row * height, width, height)
-      cell.node.id = "cell_" + row + "_" + column
-      cell.attr(stroke: "#d8d8d8").data("row", row).data("column", column).click ->
-        cell_data = world[@data("row")][@data("column")]
-        cell_data.alive = !cell_data.alive
-        if cell_data.alive then @attr fill: alive_color else @attr fill: "#ffffff"
-
-    for i in [0...num_rows]
-      row = []
-      row.push({ alive : false }) for [0...num_columns]
-      world.push(row)
-
-    do createRaphaelGrid = ->
+    constructor: (num_rows, num_columns, canvas_width, canvas_height) ->
+      @grid = []
       grid_cell_height = canvas_height / num_rows
       grid_cell_width = canvas_width / num_columns
+
+      createRaphaelCell = (row, column, height, width) ->
+        cell = paper.rect(column * width, row * height, width, height)
+        cell.node.id = "cell_" + row + "_" + column
+        cell.attr(stroke: "#d8d8d8").data("row", row).data("column", column).click ->
+          cell_data = world.grid[@data("row")][@data("column")]
+          cell_data.alive = !cell_data.alive
+          if cell_data.alive then @attr fill: alive_color else @attr fill: "#ffffff"
+
+      for i in [0...num_rows]
+        row = []
+        row.push({ alive : false }) for [0...num_columns]
+        @grid.push(row)
 
       for j in [0...num_rows]
         createRaphaelCell j, k, grid_cell_height, grid_cell_width for k in [0...num_columns]
 
+    getCellByCoord: (row, column) ->
+      cell_id_string = "cell_" + row + "_" + column
+      $("#" + cell_id_string)
+
+    drawCell: (row, column, alive) ->
+      cell = @getCellByCoord(row, column)
+      if alive then cell.attr fill: alive_color else cell.attr fill: "#ffffff"
+
+    draw: ->
+      @drawCell(j, k, world.grid[j][k].alive) for k in [0...num_columns] for j in [0...num_rows]
+
+    updateCells: (results_grid) ->
+      for j in [0...results_grid.length]
+        arr = []
+        world.grid[j][k].alive = results_grid[j][k] for k in [0...results_grid[j].length]
+
+  ### Functions ###
+
   populateWorld = ->
 
     #GLIDER
-    world[0][8].alive = true
-    world[1][9].alive = true
-    world[2][9].alive = true
-    world[2][8].alive = true
-    world[2][7].alive = true
+    world.grid[0][8].alive = true
+    world.grid[1][9].alive = true
+    world.grid[2][9].alive = true
+    world.grid[2][8].alive = true
+    world.grid[2][7].alive = true
 
     #BEEHIVES
-    world[13][22].alive = true
-    world[14][21].alive = true
-    world[14][23].alive = true
-    world[15][21].alive = true
-    world[15][23].alive = true
-    world[16][22].alive = true
-    world[16][23].alive = true
+    world.grid[13][22].alive = true
+    world.grid[14][21].alive = true
+    world.grid[14][23].alive = true
+    world.grid[15][21].alive = true
+    world.grid[15][23].alive = true
+    world.grid[16][22].alive = true
+    world.grid[16][23].alive = true
 
   calculateIfCellAlive = (row, column) ->
 
@@ -86,54 +94,43 @@ $ ->
           unless row_position is 0 and column_position is 0
             cell_row = wrapped(row + row_position, num_rows)
             cell_column = wrapped(column + column_position, num_columns)
-            count += (if world[cell_row][cell_column].alive then 1 else 0)
+            count += (if world.grid[cell_row][cell_column].alive then 1 else 0)
       count
 
     count = countNeighbors()
-    if world[row][column].alive then (2 <= count <= 3) else (count == 3)
+    if world.grid[row][column].alive then (2 <= count <= 3) else (count == 3)
 
-  determineNextGenerationCellStates = ->
+  getNextGenerationCellStates = ->
 
-    grid = []
+    next_gen_grid = []
 
-    for j in [0...world.length]
+    for j in [0...world.grid.length]
       arr = []
-      arr.push calculateIfCellAlive(j, k) for k in [0...world[j].length]
-      grid.push arr
+      arr.push calculateIfCellAlive(j, k) for k in [0...world.grid[j].length]
+      next_gen_grid.push arr
 
-    grid
-
-  updateCells = (results_grid) ->
-
-    for j in [0...results_grid.length]
-      arr = []
-      world[j][k].alive = results_grid[j][k] for k in [0...results_grid[j].length]
+    next_gen_grid
 
   updateWorld = ->
     world.draw()
-    results_grid = determineNextGenerationCellStates()
-    updateCells(results_grid)
+    world.updateCells(getNextGenerationCellStates())
+
+  setPauseListener = ->
+    $(window).keydown (event) ->
+      if event.keyCode is 80
+        if paused
+          paused = !paused
+          runWorld()
+        else
+          paused = !paused
 
   ### Main ###
 
-  # Set up pause listener on the "p" key
-  $(window).keydown (event) ->
-    if event.keyCode is 80
-      if paused
-        paused = !paused
-        runWorld()
-      else
-        paused = !paused
-
-  generateWorld()
+  setPauseListener()
+  world = new World num_rows, num_columns, canvas_width, canvas_height
   populateWorld()
 
   do runWorld = ->
     updateWorld()
     unless paused
       setTimeout runWorld, tick_interval
-
-# turn world into a class with constructor
-# make updateCells a method on world
-# move event key press listener into own function
-# turn cell matchers to be based on data attribute instead of id name
--- game_of_life.js
+++ game_of_life.js
@@ -8,7 +8,7 @@ Toshiro Ken Sugihara 2013
   $(function() {
     /* Variables*/
 
-    var alive_color, calculateIfCellAlive, canvas_height, canvas_width, determineNextGenerationCellStates, generateWorld, num_columns, num_rows, paper, paused, populateWorld, runWorld, tick_interval, updateCells, updateWorld, world;
+    var World, alive_color, calculateIfCellAlive, canvas_height, canvas_width, getNextGenerationCellStates, num_columns, num_rows, paper, paused, populateWorld, runWorld, setPauseListener, tick_interval, updateWorld, world;
     num_rows = 30;
     num_columns = 48;
     canvas_width = 480;
@@ -17,17 +17,59 @@ Toshiro Ken Sugihara 2013
     tick_interval = 50;
     paper = Raphael("canvas", canvas_width, canvas_height);
     alive_color = "#50c0a8";
-    world = [];
-    /* Functions*/
+    world = null;
+    /* Classes*/
 
-    generateWorld = function() {
-      var createRaphaelCell, createRaphaelGrid, i, row, _i, _j;
-      world.getCellByCoord = function(row, column) {
+    World = (function() {
+      function World(num_rows, num_columns, canvas_width, canvas_height) {
+        var createRaphaelCell, grid_cell_height, grid_cell_width, i, j, k, row, _i, _j, _k, _l;
+        this.grid = [];
+        grid_cell_height = canvas_height / num_rows;
+        grid_cell_width = canvas_width / num_columns;
+        createRaphaelCell = function(row, column, height, width) {
+          var cell;
+          cell = paper.rect(column * width, row * height, width, height);
+          cell.node.id = "cell_" + row + "_" + column;
+          return cell.attr({
+            stroke: "#d8d8d8"
+          }).data("row", row).data("column", column).click(function() {
+            var cell_data;
+            cell_data = world.grid[this.data("row")][this.data("column")];
+            cell_data.alive = !cell_data.alive;
+            if (cell_data.alive) {
+              return this.attr({
+                fill: alive_color
+              });
+            } else {
+              return this.attr({
+                fill: "#ffffff"
+              });
+            }
+          });
+        };
+        for (i = _i = 0; 0 <= num_rows ? _i < num_rows : _i > num_rows; i = 0 <= num_rows ? ++_i : --_i) {
+          row = [];
+          for (_j = 0; 0 <= num_columns ? _j < num_columns : _j > num_columns; 0 <= num_columns ? _j++ : _j--) {
+            row.push({
+              alive: false
+            });
+          }
+          this.grid.push(row);
+        }
+        for (j = _k = 0; 0 <= num_rows ? _k < num_rows : _k > num_rows; j = 0 <= num_rows ? ++_k : --_k) {
+          for (k = _l = 0; 0 <= num_columns ? _l < num_columns : _l > num_columns; k = 0 <= num_columns ? ++_l : --_l) {
+            createRaphaelCell(j, k, grid_cell_height, grid_cell_width);
+          }
+        }
+      }
+
+      World.prototype.getCellByCoord = function(row, column) {
         var cell_id_string;
         cell_id_string = "cell_" + row + "_" + column;
         return $("#" + cell_id_string);
       };
-      world.drawCell = function(row, column, alive) {
+
+      World.prototype.drawCell = function(row, column, alive) {
         var cell;
         cell = this.getCellByCoord(row, column);
         if (alive) {
@@ -40,7 +82,8 @@ Toshiro Ken Sugihara 2013
           });
         }
       };
-      world.draw = function() {
+
+      World.prototype.draw = function() {
         var j, k, _i, _results;
         _results = [];
         for (j = _i = 0; 0 <= num_rows ? _i < num_rows : _i > num_rows; j = 0 <= num_rows ? ++_i : --_i) {
@@ -48,74 +91,49 @@ Toshiro Ken Sugihara 2013
             var _j, _results1;
             _results1 = [];
             for (k = _j = 0; 0 <= num_columns ? _j < num_columns : _j > num_columns; k = 0 <= num_columns ? ++_j : --_j) {
-              _results1.push(this.drawCell(j, k, world[j][k].alive));
+              _results1.push(this.drawCell(j, k, world.grid[j][k].alive));
             }
             return _results1;
           }).call(this));
         }
         return _results;
       };
-      createRaphaelCell = function(row, column, height, width) {
-        var cell;
-        cell = paper.rect(column * width, row * height, width, height);
-        cell.node.id = "cell_" + row + "_" + column;
-        return cell.attr({
-          stroke: "#d8d8d8"
-        }).data("row", row).data("column", column).click(function() {
-          var cell_data;
-          cell_data = world[this.data("row")][this.data("column")];
-          cell_data.alive = !cell_data.alive;
-          if (cell_data.alive) {
-            return this.attr({
-              fill: alive_color
-            });
-          } else {
-            return this.attr({
-              fill: "#ffffff"
-            });
-          }
-        });
-      };
-      for (i = _i = 0; 0 <= num_rows ? _i < num_rows : _i > num_rows; i = 0 <= num_rows ? ++_i : --_i) {
-        row = [];
-        for (_j = 0; 0 <= num_columns ? _j < num_columns : _j > num_columns; 0 <= num_columns ? _j++ : _j--) {
-          row.push({
-            alive: false
-          });
-        }
-        world.push(row);
-      }
-      return (createRaphaelGrid = function() {
-        var grid_cell_height, grid_cell_width, j, k, _k, _results;
-        grid_cell_height = canvas_height / num_rows;
-        grid_cell_width = canvas_width / num_columns;
+
+      World.prototype.updateCells = function(results_grid) {
+        var arr, j, k, _i, _ref, _results;
         _results = [];
-        for (j = _k = 0; 0 <= num_rows ? _k < num_rows : _k > num_rows; j = 0 <= num_rows ? ++_k : --_k) {
+        for (j = _i = 0, _ref = results_grid.length; 0 <= _ref ? _i < _ref : _i > _ref; j = 0 <= _ref ? ++_i : --_i) {
+          arr = [];
           _results.push((function() {
-            var _l, _results1;
+            var _j, _ref1, _results1;
             _results1 = [];
-            for (k = _l = 0; 0 <= num_columns ? _l < num_columns : _l > num_columns; k = 0 <= num_columns ? ++_l : --_l) {
-              _results1.push(createRaphaelCell(j, k, grid_cell_height, grid_cell_width));
+            for (k = _j = 0, _ref1 = results_grid[j].length; 0 <= _ref1 ? _j < _ref1 : _j > _ref1; k = 0 <= _ref1 ? ++_j : --_j) {
+              _results1.push(world.grid[j][k].alive = results_grid[j][k]);
             }
             return _results1;
           })());
         }
         return _results;
-      })();
-    };
+      };
+
+      return World;
+
+    })();
+    /* Functions*/
+
     populateWorld = function() {
-      world[0][8].alive = true;
-      world[1][9].alive = true;
-      world[2][9].alive = true;
-      world[2][8].alive = true;
-      world[2][7].alive = true;
-      world[13][22].alive = true;
-      world[14][21].alive = true;
-      world[14][23].alive = true;
-      world[15][21].alive = true;
-      world[15][23].alive = true;
-      world[16][22].alive = true;
-      return world[16][23].alive = true;
+      world.grid[0][8].alive = true;
+      world.grid[1][9].alive = true;
+      world.grid[2][9].alive = true;
+      world.grid[2][8].alive = true;
+      world.grid[2][7].alive = true;
+      world.grid[13][22].alive = true;
+      world.grid[14][21].alive = true;
+      world.grid[14][23].alive = true;
+      world.grid[15][21].alive = true;
+      world.grid[15][23].alive = true;
+      world.grid[16][22].alive = true;
+      return world.grid[16][23].alive = true;
     };
     calculateIfCellAlive = function(row, column) {
       var count, countNeighbors, wrapped;
@@ -140,66 +158,51 @@ Toshiro Ken Sugihara 2013
             if (!(row_position === 0 && column_position === 0)) {
               cell_row = wrapped(row + row_position, num_rows);
               cell_column = wrapped(column + column_position, num_columns);
-              count += (world[cell_row][cell_column].alive ? 1 : 0);
+              count += (world.grid[cell_row][cell_column].alive ? 1 : 0);
             }
           }
         }
         return count;
       };
       count = countNeighbors();
-      if (world[row][column].alive) {
+      if (world.grid[row][column].alive) {
         return (2 <= count && count <= 3);
       } else {
         return count === 3;
       }
     };
-    determineNextGenerationCellStates = function() {
-      var arr, grid, j, k, _i, _j, _ref, _ref1;
-      grid = [];
-      for (j = _i = 0, _ref = world.length; 0 <= _ref ? _i < _ref : _i > _ref; j = 0 <= _ref ? ++_i : --_i) {
+    getNextGenerationCellStates = function() {
+      var arr, j, k, next_gen_grid, _i, _j, _ref, _ref1;
+      next_gen_grid = [];
+      for (j = _i = 0, _ref = world.grid.length; 0 <= _ref ? _i < _ref : _i > _ref; j = 0 <= _ref ? ++_i : --_i) {
         arr = [];
-        for (k = _j = 0, _ref1 = world[j].length; 0 <= _ref1 ? _j < _ref1 : _j > _ref1; k = 0 <= _ref1 ? ++_j : --_j) {
+        for (k = _j = 0, _ref1 = world.grid[j].length; 0 <= _ref1 ? _j < _ref1 : _j > _ref1; k = 0 <= _ref1 ? ++_j : --_j) {
           arr.push(calculateIfCellAlive(j, k));
         }
-        grid.push(arr);
+        next_gen_grid.push(arr);
       }
-      return grid;
-    };
-    updateCells = function(results_grid) {
-      var arr, j, k, _i, _ref, _results;
-      _results = [];
-      for (j = _i = 0, _ref = results_grid.length; 0 <= _ref ? _i < _ref : _i > _ref; j = 0 <= _ref ? ++_i : --_i) {
-        arr = [];
-        _results.push((function() {
-          var _j, _ref1, _results1;
-          _results1 = [];
-          for (k = _j = 0, _ref1 = results_grid[j].length; 0 <= _ref1 ? _j < _ref1 : _j > _ref1; k = 0 <= _ref1 ? ++_j : --_j) {
-            _results1.push(world[j][k].alive = results_grid[j][k]);
-          }
-          return _results1;
-        })());
-      }
-      return _results;
+      return next_gen_grid;
     };
     updateWorld = function() {
-      var results_grid;
       world.draw();
-      results_grid = determineNextGenerationCellStates();
-      return updateCells(results_grid);
+      return world.updateCells(getNextGenerationCellStates());
+    };
+    setPauseListener = function() {
+      return $(window).keydown(function(event) {
+        if (event.keyCode === 80) {
+          if (paused) {
+            paused = !paused;
+            return runWorld();
+          } else {
+            return paused = !paused;
+          }
+        }
+      });
     };
     /* Main*/
 
-    $(window).keydown(function(event) {
-      if (event.keyCode === 80) {
-        if (paused) {
-          paused = !paused;
-          return runWorld();
-        } else {
-          return paused = !paused;
-        }
-      }
-    });
-    generateWorld();
+    setPauseListener();
+    world = new World(num_rows, num_columns, canvas_width, canvas_height);
     populateWorld();
     return (runWorld = function() {
       updateWorld();
